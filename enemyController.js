6a2c845c1ed46a57a060ee602fa576ac6e5413ea
CSmith3s <CSmith3s@capetigers.com>
2024-04-18 10:26:47 -0500

    enemy controller

--- enemyController.js
+++ enemyController.js
@@ -1,1 +1,21 @@
-export default class EnemyController {}
+export default class EnemyController {
+
+    enemyMap = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,],
+        [2, 2, 2, 3, 3, 3, 3, 2, 2, 2],
+        [2, 2, 2, 3, 3, 3, 3, 2, 2, 2]
+    ];
+    enemyRows = [];
+
+    constructor(canvas) {
+        this.canvas = canvas
+    }
+
+    draw(ctx) {}
+
+    createEnemies() {
+        this.enemyMap.forEach((row, rowIndex))
+        // contiue here
+    }
+}
